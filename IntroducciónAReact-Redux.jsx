8a63b3aa6a2d6865491723ecfa06220dcbc00be6
alberto006-esp <albertosuarezolvera@gmail.com>
2022-08-13 14:04:55 +0200

    Introducción a React Redux

---
+++ IntroducciónAReact-Redux.jsx
@@ -0,0 +1,13 @@
+class DisplayMessages extends React.Component {
+    //Añadimos un constructor a este componente y lo inicializamos con un estado que tenga dos propiedades input y messages
+    constructor(props){
+      super(props);
+      this.state = {
+         input: "",
+         messages: [] 
+      }
+    }
+    render() {
+      return <div />
+    }
+  };
